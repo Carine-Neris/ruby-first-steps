e68652f579e8f0607ebcc022c22143f7c3aa0d81
Carine Neris <carine_neris@hotmail.com>
2022-08-11 22:34:52 -0300

    adicionado comentarios ao exercicio 6

--- exe6.rb
+++ exe6.rb
@@ -1,15 +1,20 @@
+# Declarando um número inteiro a uma váriavel
 types_of_people = 10
+
+# Atribuindo uma string que contém uma váriavel em sua formação
 x = "There are #{types_of_people} types of people."
 binary = "binary"
 do_not = "don't"
 y = "Those who know #{binary} and those who #{do_not}."
 
+#printando as variaveis
 puts x
 puts y
 
 puts "I said: #{x}."
 puts "I also said: '#{y}'."
 
+# atribuindo um valor booleando a uma variavel
 hilarious = false
 joke_evaluation = "Isn't that joke so funny?! #{hilarious}"
 
@@ -18,4 +23,5 @@ puts joke_evaluation
 w = "This is the left side of..."
 e = "a string with a right side."
 
+#Realizando uma concatenação de strings
 puts w + e
